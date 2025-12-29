ab9368ba1827596c2dfd179dfb4d35a07cc00864
ctCucuta <ctecnologico64@gmail.com>
2025-12-29 12:18:27 -0500

    se agrega enlace

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001/api",
-  // baseURL: "https://megastoreback.onrender.com/api",
+  // baseURL: "http://localhost:3001/api",
+  baseURL: "https://megastoreback.onrender.com/api",
 });
 
 // 🔹 Interceptor de REQUEST (ya lo tienes)
