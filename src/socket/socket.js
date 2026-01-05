00fa9b9e1a9a460eeeac1ed41c676df85aa56bc3
ctCucuta <ctecnologico64@gmail.com>
2026-01-05 15:08:07 -0500

    se agregan socket.io

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,18 +1,18 @@
-// import { io } from "socket.io-client";
-
-// const socket = io("https://api.k-dice.com", {
-//   withCredentials: true,
-//   transports: ["websocket"]
-// });
-
-// export default socket;
-
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3001", {
+const socket = io("https://api.k-dice.com", {
   withCredentials: true,
   transports: ["websocket"]
 });
 
 export default socket;
+
+// import { io } from "socket.io-client";
+
+// const socket = io("http://localhost:3001", {
+//   withCredentials: true,
+//   transports: ["websocket"]
+// });
+
+// export default socket;
 
